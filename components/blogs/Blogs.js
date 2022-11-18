11dbb89833db3427e1d08e3e3d6d84fcbd1d8f7a
grishmakhanolkar <grishmakhanolkar@gmail.com>
2022-11-19 01:19:04 +0530

    blog css updated

--- components/blogs/Blogs.js
+++ components/blogs/Blogs.js
@@ -4,40 +4,44 @@ function Blogs(myBlogs) {
   const { blogs } = myBlogs;
   return (
     <section id="blogs" className={styles.blogs}>
-      <div className={styles.blogsInfo}>
-        <h1 class="mx-1 mb-4 fw-bold mt-5">My Blogs</h1>
-        <p class="fs-5 fw-light mb-5">Here are some of my recent blog posts.</p>
-      </div>
-      {blogs.map((blog) => (
-        <div key={blog.title} className={styles.blogsCard}>
-          <div class="card p-3 shadow">
-            <h2>{blog.title}</h2>
-            <div class="d-inline-flex flex-row flex-wrap justify-content-center mx-auto ">
-              {blog.categories.map((item) => (
-                <div key={item} class="m-2 p-2 border border-dark rounded">
-                  {item}
-                </div>
-              ))}
-            </div>
-            <a
-              href={blog.link}
-              target="blank"
-              type="button"
-              class="btn btn-outline-primary mx-auto mt-3 mb-3 rounded-pill"
-            >
-              Read article
-            </a>
-          </div>
+      <div className={styles.blogsLayout}>
+        <div className={styles.blogsInfo}>
+          <h1 class="mx-1 mb-4 fw-bold mt-5">My Blogs</h1>
+          <p class="fs-5 fw-light mb-5">
+            Here are some of my recent blog posts.
+          </p>
         </div>
-      ))}
-      <a
-        href="https://medium.com/@grishmakhanolkar"
-        target="blank"
-        type="button"
-        class="btn btn-outline-primary mx-auto mt-3 mb-3 rounded-pill"
-      >
-        Read more blogs on Medium
-      </a>
+        {blogs.map((blog) => (
+          <div key={blog.title} className={styles.blogsCard}>
+            <div class="card p-3 shadow">
+              <h2>{blog.title}</h2>
+              <div class="d-inline-flex flex-row flex-wrap justify-content-center mx-auto ">
+                {blog.categories.map((item) => (
+                  <div key={item} class="m-2 p-2 border border-dark rounded">
+                    {item}
+                  </div>
+                ))}
+              </div>
+              <a
+                href={blog.link}
+                target="blank"
+                type="button"
+                class="btn btn-outline-primary mx-auto mt-3 mb-3 rounded-pill"
+              >
+                Read article
+              </a>
+            </div>
+          </div>
+        ))}
+        <a
+          href="https://medium.com/@grishmakhanolkar"
+          target="blank"
+          type="button"
+          class="btn btn-outline-primary mx-auto mt-3 mb-3 rounded-pill"
+        >
+          Read more blogs on Medium
+        </a>
+      </div>
       <hr />
     </section>
   );
