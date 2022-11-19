857a7829c3b7e38f5d85f65c5b3aa648357c8591
grishmakhanolkar <grishmakhanolkar@gmail.com>
2022-11-19 11:48:52 +0530

    Analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import Script from 'next/script';
+import { Analytics } from '@vercel/analytics/react';
 import { useEffect } from 'react';
 import '../styles/globals.scss';
 
@@ -8,10 +10,21 @@ function MyApp({ Component, pageProps }) {
   }, []);
   return (
     <>
+      {/* Google Analytics */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-B5Y04SM9X7"
+      ></Script>
+      <Script id="Google Analytics">
+        {`window.dataLayer = window.dataLayer || []; function gtag()
+        {window.dataLayer.push(arguments);}
+        gtag('js', new Date()); gtag('config', 'G-B5Y04SM9X7');`}
+      </Script>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
