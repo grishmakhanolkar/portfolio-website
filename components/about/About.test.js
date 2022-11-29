2798487c8b1dfb5283328cd0b1d25f64de26c99f
Grishma <grishmakhanolkar@gmail.com>
2022-11-29 12:25:58 +0530

    code formatted

--- components/about/About.test.js
+++ components/about/About.test.js
@@ -1,28 +1,7 @@
-
-// import {render} from '@testing-library/react'
-// import '@testing-library/jest-dom'
-// // import React from 'react';
-// import About from './About'
-
-// test('expect to render about component', () =>{
-//     const {container} = render(<About/>);
-//     expect(container).toMatchSnapshot()
-// })
-
-import { render } from '@testing-library/react'
-import About from './About'
-import '@testing-library/jest-dom'
-
+import { render } from '@testing-library/react';
+import About from './About';
+import '@testing-library/jest-dom';
 
 it('loads and displays about', async () => {
-  // ARRANGE
-
-  expect(render(<About />)).toMatchSnapshot()
-  // ACT
-  //   await userEvent.click(screen.getByText('Load Greeting'))
-  //   await screen.findByRole('heading')
-
-  // ASSERT
-  // expect(screen.getByRole('heading')).toHaveTextContent('hello there')
-  // expect(screen.getByRole('button')).toBeDisabled()
-})
+  expect(render(<About />)).toMatchSnapshot();
+});
