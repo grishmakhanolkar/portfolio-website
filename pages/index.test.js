de4b704534dadda7074a6cfca4522eaec576fe2a
Grishma <grishmakhanolkar@gmail.com>
2022-11-29 12:00:20 +0530

    eslint updated with jest

--- pages/index.test.js
+++ pages/index.test.js
@@ -1,48 +1,46 @@
-import { render } from '@testing-library/react'
-import Home from './index'
-import '@testing-library/jest-dom'
+import { render } from '@testing-library/react';
+import Home from './index';
+import '@testing-library/jest-dom';
 
 const mockBlogs = [
-    {
-        title: 'Firebase for Login and Logout— Part 1',
-        link: 'https://medium.com/p/c99d0022a467',
-        categories: [
-            'firebaseauthentication',
-            'firestore',
-            'google-oauth',
-            'authentication',
-            'firebase',
-        ],
-    },
-    {
-        title: 'React Router v6',
-        link: 'https://medium.com/p/5736a7718335',
-        categories: ['react', 'react-router-v6'],
-    },
-    {
-        title: 'Navigating to Nested React Components',
-        link: 'https://medium.com/p/d4f68331537',
-        categories: [
-            'javascript',
-            'react',
-            'programming',
-            'material-design',
-            'portfolio',
-        ]
-    }
-]
+  {
+    title: 'Firebase for Login and Logout— Part 1',
+    link: 'https://medium.com/p/c99d0022a467',
+    categories: [
+      'firebaseauthentication',
+      'firestore',
+      'google-oauth',
+      'authentication',
+      'firebase',
+    ],
+  },
+  {
+    title: 'React Router v6',
+    link: 'https://medium.com/p/5736a7718335',
+    categories: ['react', 'react-router-v6'],
+  },
+  {
+    title: 'Navigating to Nested React Components',
+    link: 'https://medium.com/p/d4f68331537',
+    categories: [
+      'javascript',
+      'react',
+      'programming',
+      'material-design',
+      'portfolio',
+    ],
+  },
+];
 
-const mockGotBlogs = true
+const mockGotBlogs = true;
 
 it('loads and displays home', async () => {
-
-
-    expect(render(<Home blogs={mockBlogs} gotBlogs={mockGotBlogs} />)).toMatchSnapshot()
-
-})
+  expect(
+    render(<Home blogs={mockBlogs} gotBlogs={mockGotBlogs} />)
+  ).toMatchSnapshot();
+});
 it('loads and displays home', async () => {
-
-
-    expect(render(<Home blogs={mockBlogs} gotBlogs={!mockGotBlogs} />)).toMatchSnapshot()
-
-})
+  expect(
+    render(<Home blogs={mockBlogs} gotBlogs={!mockGotBlogs} />)
+  ).toMatchSnapshot();
+});
